575cdb395763672a40995f315fe4fd467f00b279
RoyRongStevens <yrong2@stevens.edu>
2018-08-06 14:08:02 -0400

    test

---
+++ A1T1.java
@@ -0,0 +1,18 @@
+package com.edureka.advancedjava.module1;
+
+public class A1T1 {
+
+	  public A1T1() {}
+	  
+	  public static void main(String[] args) { 
+	    int x = 9;
+	    
+	    int i = 0;
+	    while (i <= 10)
+	    {
+	      System.out.println(x + " * " + i + " = " + i * x);
+	      i++;
+	    }
+	  }
+
+}
